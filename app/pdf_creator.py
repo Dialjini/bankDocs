54337739852bddc29afdd57b3ad426a3e6b2f098
Popokatepetl007 <temik007@mail.ru>
2020-04-11 22:56:43 +0700

    frst commit

--- app/pdf_creator.py
+++ app/pdf_creator.py
@@ -2,13 +2,16 @@ from xhtml2pdf import pisa
 import os
 from flask import render_template, Flask
 import codecs
+from io import StringIO
+
 
 
 html = codecs.open(os.path.dirname(__file__) + '/files/AlphaBank/index.html', 'r', encoding='utf-8')
 test = html.read().replace('{{ path }}', os.path.dirname(__file__) + '/files/AlphaBank/')
 # print(test)
 print(os.path.dirname(__file__))
-with open(os.path.dirname(__file__) + '/files/downloaded.pdf', 'wb') as out:
-    pisa.CreatePDF(src=test, dest=out, encoding='utf-8')
+pisa.CreatePDF(StringIO(test), dest=open('downloaded.pdf', 'w+b'), encoding='utf-8')
+# with open(os.path.dirname(__file__) + '/files/downloaded.pdf', 'wb') as out:
+#     pisa.CreatePDF(StringIO(test), dest=out, encoding='utf-8')
 
 html.close()
